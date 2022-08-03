feccb8c12b9879428af5b38cb13289af6d229dbc
fahsaaulanh <fahsaaula8@gmail.com>
2022-08-03 21:06:18 +0700

    Tugas Pra Dilat BDI Pong Base Gameplay

---
+++ Assets/Script/BolaController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaController : MonoBehaviour
+{
+    public Vector2 speed;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = speed;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/PaddleController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upkey, downkey;
+
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveObject(GetInput());
+    }
+
+    public Vector2 GetInput ()
+    {
+        
+        if (Input.GetKey(upkey))
+        {
+           return Vector2.up * speed;
+        }
+        else if (Input.GetKey(downkey))
+        {
+            return Vector2.down * speed;
+        }
+
+        return Vector2.zero;
+
+    }
+
+    public void MoveObject(Vector2 movement)
+    {
+        rb.velocity = movement;
+    }
+}
